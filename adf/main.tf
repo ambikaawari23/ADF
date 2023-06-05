fbd3bf002e5a3ab30aa29687fbe850c5baf4ac74
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-06-05 19:13:15 +0530

    Update main.tf

--- adf/main.tf
+++ adf/main.tf
@@ -6,65 +6,27 @@ locals {
    "adf02" = "adf12401"
   }
 }
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = "ADF-0124"
   location = "West Europe"
 }
 
-resource "azurerm_key_vault" "example" {
-    for_each = local.strengths
-    name  = each.value
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-}
+resource "azurerm_data_factory" "adfaa" {
+#   for_each            = local.strengths
+  name                = "adf23-"${{ var.environment }}
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-resource "azurerm_storage_account" "example" {
-    for_each = local.strengths
-    name  = each.value
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "RAGRS"
-}
-
-resource "azurerm_role_assignment" "example" {
-   for_each = local.strengths
-  scope                = azurerm_storage_account.example[each.value]
-  role_definition_name = "Storage Blob Data Reader"
-  principal_id         = azurerm_data_factory.example[each.value]
-}
-
-resource "azurerm_data_factory" "example" {
-  for_each = local.strengths
-  name  = each.value
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  github_configuration {
-    account_name = "Ambika-Awari"
-    branch_name = "main"
-    git_url = "https://github.com/Ambika-Awari/adf"
-    repository_name = "adf"
-    root_folder = "/"
+  identity {
+    type = "SystemAssigned"
   }
-
-     identity {
-     type = "SystemAssigned"
-   }
-}
-
-resource "azurerm_data_factory_linked_service_key_vault" "example" {
-    for_each = local.strengths
-    name  = each.value
-  data_factory_id = azurerm_data_factory.example.id[each.value]
-  key_vault_id    = azurerm_key_vault.example.id[each.value]
-}
-
-resource "azurerm_data_factory_linked_service_azure_blob_storage" "example" {
-      for_each = local.strengths
-      name  = each.value
-     data_factory_id   = azurerm_data_factory.example.id[each.value]
-    connection_string =  azurerm_storage_account.example.[each.value]
-
-}
+  dynamic "github_configuration" {
+    for_each = var.environment == "dev" ? [1] : []
+    content {
+      account_name     = "Ambika-Awari"
+      branch_name      = "main"
+      git_url          = "https://github.com/Ambika-Awari/adf"
+      repository_name  = "adf"
+      root_folder      = "/"
+    }
+  }
